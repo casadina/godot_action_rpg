c5bfbf80436107b46d3bc3911e55a7d073342568
Casadina <casadina@tuta.io>
2021-12-15 22:47:12 +0800

    Learn about export variables, typing, setget, signals and inheritance. Learn that signals basically are @State or @property. Learn that export var allows you to change variables in game for testing.
    Add Stats node to track hitpoints.
    Use export variables to allow for other classes to call on them.
    Set Bat to remove itself after hp reach 0 or below based on signals.

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 var knockback = Vector2.ZERO
 
+onready var stats = $Stats
+
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
@@ -9,3 +11,8 @@ func _physics_process(delta):
 func _on_Hurtbox_area_entered(area):
 	if area.get_name() == "SwordHitbox":
 		knockback = area.knockback_vector * 120
+		stats.health -= area.damage
+
+func _on_Stats_no_health():
+	queue_free()
+	
--- Player/sword_hitbox.gd
+++ Player/sword_hitbox.gd
@@ -1,3 +1,3 @@
-extends Area2D
+extends "res://Overlap/hitbox.gd"
 
-var knockback_vector = (Vector2.ZERO)
+var knockback_vector = Vector2.ZERO
